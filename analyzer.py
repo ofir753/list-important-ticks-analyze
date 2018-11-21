1d57dc8a8a06569ceb8c367eacc5d1949aba3d20
Ofir Gal <ofirgal753@gmail.com>
2018-11-21 20:17:29 +0200

    Added round details feature

--- analyzer.py
+++ analyzer.py
@@ -8,17 +8,23 @@ from Rounds.Utils import *
 def main():
 	parser = ArgumentParser(description='Analayze demo from csgo by demo_listimportantticks')
 	parser.add_argument('file', type=str, help='important ticks file')
+	parser.add_argument('-r', '--round', type=int, help='print specific round flow')
 
 	args = parser.parse_args()
 
-	analyze(args.file)
+	rounds = file_to_rounds(args.file)
 
-def analyze(filename):
-	rounds = file_to_rounds(filename)
+	if args.round:
+		round = rounds[args.round]
 
-	print "#RoundNum, Round in ticks\tplayer name x amount of kills (amount of headshots)"
-	for round in rounds:
-		print "%s \t%s" % (round, round_to_kills(round))
+		print "\n%s \t%s\n" % (round, round_to_kills(round))
+		for event in round.get_real_events():
+			print event
+
+	else:
+		print "#RoundNum, Round in ticks\tplayer name x amount of kills (amount of headshots)"
+		for round in rounds:
+			print "%s \t%s" % (round, round_to_kills(round))
 
 if __name__ == "__main__":
 	main()
